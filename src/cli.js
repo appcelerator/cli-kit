appcelerator/cli-kit

commit 24f5048633d492a05c6245dc7ef3de7e476c43aa

fix: Removed global terminal instance in favor of unique Terminal instances per CLI instance. fix(cli): Properly exposed 'helpTemplateFile' CLI option. fix(cli): Removed 'cli-kit' reference from exec() results. fix(cli): Properly add original unparsed '_argv' and parsed arguments '__argv' to exec() results. fix(ansi): Fixed typo in save/restore ansi sequence. fix(extension): Extension args are appended at execution time rather than cached at parse time. fix(parser): Moved options from parse() to constructor. fix(parser): Reparse arguments if command/options were added. fix(terminal): Set max listeners on 'stdout' to infinity because it's global and accessed by multiple CLI instances. fix(terminal): Added semaphore around enabling of stdin raw mode. feat(extension): Added 'node-pty' for better subprocess I/O handling. feat(ansi): Added 'esc' sequance and cursor position request regex. feat(cli): Added 'listen()' and 'connect()' for remote parsing and execution. feat(cli): Added 'clone' option to exec(). feat(context): Added 'rev' to identify when new options/commands are added. chore: Updated npm deps. chore(site): Updated terminal example. chore: Cleaned up unused variables. chore(test): Cleaned up a few bad tests.

src/cli.js
@@ -10,12 +10,29 @@ import path from 'path';
 import pluralize from 'pluralize';
 import semver from 'semver';
 import Terminal from './terminal';
+import WebSocket, { Server as WebSocketServer } from 'ws';
 
-import { declareCLIKitClass } from './lib/util';
-import { terminal } from './index';
+import * as ansi from './lib/ansi';
+
+import { declareCLIKitClass, split } from './lib/util';
+import { Writable } from 'stream';
 
 const { error, log, warn } = debug('cli-kit:cli');
 const { highlight }  = debug.styles;
+
+/**
+ * Writes data to a websocket.
+ */
+class OutputSocket extends Writable {
+	constructor(ws) {
+		super();
+		this.ws = ws;
+	}
+
+	write(chunk) {
+		this.ws.send(chunk.replace(/(?<!\r)\n/g, '\r\n'));
+	}
+}
 
 /**
  * The required Node.js version for cli-kit. This is used to assert the Node version at runtime.
@@ -51,6 +68,8 @@ export default class CLI extends Context {
 	 * @param {Boolean} [params.help=false] - When `true`, enables the built-in help command.
 	 * @param {Number} [params.helpExitCode] - The exit code to return when the help command is
 	 * finished.
+	 * @param {String} [params.helpTemplateFile] - Path to a template to render for the help
+	 * command.
 	 * @param {Boolean} [params.hideNoBannerOption] - When `true` and a `banner` is specified, it
 	 * does not add the `--no-banner` option.
 	 * @param {Boolean} [params.hideNoColorOption] - When `true` and `colors` is enabled, it does
@@ -121,12 +140,13 @@ export default class CLI extends Context {
 		this.errorIfUnknownCommand     = params.errorIfUnknownCommand !== false;
 		this.help                      = !!params.help;
 		this.helpExitCode              = params.helpExitCode;
+		this.helpTemplateFile          = params.helpTemplateFile;
 		this.hideNoBannerOption        = params.hideNoBannerOption;
 		this.hideNoColorOption         = params.hideNoColorOption;
 		this.nodeVersion               = params.nodeVersion;
 		this.showBannerForExternalCLIs = params.showBannerForExternalCLIs;
 		this.showHelpOnError           = params.showHelpOnError;
-		this.terminal                  = params.terminal || terminal;
+		this.terminal                  = params.terminal || new Terminal();
 		this.version                   = params.version;
 		this.warnings                  = [];
 
@@ -188,18 +208,80 @@ export default class CLI extends Context {
 	}
 
 	/**
+	 * Connects to a cli-kit WebSocket server and initializes a terminal session.
+	 *
+	 * @param {String} url - The URL to connect to.
+	 * @param {Object} [opts] - Various options.
+	 * @param {Termianl} [opts.terminal] - A terminal instance to override the default CLI terminal
+	 * instance.
+	 * @access public
+	 */
+	static connect(url, opts = {}) {
+		return new Promise(resolve => {
+			if (!opts || typeof opts !== 'object') {
+				throw new TypeError('Expected options to be an object');
+			}
+
+			let term = opts.terminal;
+
+			if (!term) {
+				term = new Terminal();
+			} else if (!(term instanceof Terminal)) {
+				throw E.INVALID_ARGUMENT('Expected terminal to be a Terminal instance', { name: 'opts.terminal', scope: 'CLI.connect', value: term });
+			}
+
+			log(`Connecting to ${highlight(url)}`);
+			const ws = new WebSocket(url);
+			ws.binaryType = 'arraybuffer';
+
+			ws.on('open', () => {
+				term.on('keypress', chunk => {
+					if (ws.readyState === 1) {
+						ws.send(chunk);
+					}
+				});
+
+				term.on('resize', ({ rows, columns }) => {
+					if (ws.readyState === 1) {
+						ws.send(`${ansi.esc}${rows};${columns}R`);
+					}
+				});
+
+				resolve();
+			});
+
+			ws.on('message', msg => {
+				if (msg === ansi.cursor.get && ws.readyState === 1) {
+					ws.send(`${ansi.esc}${term.rows};${term.columns}R`);
+				} else {
+					term.stdout.write(msg);
+				}
+			});
+
+			ws.on('close', () => process.exit());
+
+			term.on('SIGINT', () => ws.close());
+		});
+	}
+
+	/**
 	 * Parses the command line arguments and runs the command.
 	 *
-	 * @param {Array.<String>} [unparsedArgs] - An array of arguments to parse. If not specified, it
+	 * @param {Array.<String>} [_argv] - An array of arguments to parse. If not specified, it
 	 * defaults to the `process.argv` starting with the 3rd argument.
 	 * @param {Object} [opts] - Various options.
+	 * @param {Boolean} [opts.clone=false] - When `true`, deep clones the entire context tree
+	 * before each time the arguments are parsed. Enabling this only makes sense if the `CLI`
+	 * instance is going to be reused to parse arguments multiple times and if the state of the
+	 * context tree is going to be modified during parsing (i.e. via a callback).
 	 * @param {Object} [opts.data] - User-defined data to pass into the selected command.
+	 * @param {Function} [opts.exitCode] - A function that sets the exit code.
 	 * @param {Termianl} [opts.terminal] - A terminal instance to override the default CLI terminal
 	 * instance.
 	 * @returns {Promise.<Arguments>}
 	 * @access public
 	 */
-	async exec(unparsedArgs, opts = {}) {
+	async exec(_argv, opts = {}) {
 		const { version } = process;
 		let required = this.nodeVersion;
 		if ((required && !semver.satisfies(version, required)) || !semver.satisfies(version, required = clikitNodeVersion)) {
@@ -211,36 +293,61 @@ export default class CLI extends Context {
 			});
 		}
 
-		if (unparsedArgs && !Array.isArray(unparsedArgs)) {
-			throw E.INVALID_ARGUMENT('Expected arguments to be an array', { name: 'args', scope: 'CLI.exec', value: unparsedArgs });
+		if (!_argv) {
+			_argv = process.argv.slice(2);
+		} else if (!Array.isArray(_argv)) {
+			throw E.INVALID_ARGUMENT('Expected arguments to be an array', { name: 'args', scope: 'CLI.exec', value: _argv });
 		}
 
 		if (!opts || typeof opts !== 'object') {
 			throw E.INVALID_ARGUMENT('Expected opts to be an object', { name: 'opts', scope: 'CLI.exec', value: opts });
 		}
 
-		if (opts.terminal && !(opts.terminal instanceof Terminal)) {
-			throw E.INVALID_ARGUMENT('Expected terminal to be a Terminal instance', { name: 'opts.terminal', scope: 'CLI.exec', value: opts.terminal });
-		}
-
 		if (!opts.data) {
 			opts.data = {};
-		}
-
-		if (opts.data && typeof opts.data !== 'object') {
+		} else if (typeof opts.data !== 'object') {
 			throw E.INVALID_ARGUMENT('Expected data to be an object', { name: 'opts.data', scope: 'CLI.exec', value: opts.data });
 		}
 
+		if (!opts.terminal) {
+			opts.terminal = this.terminal;
+		} else if (!(opts.terminal instanceof Terminal)) {
+			throw E.INVALID_ARGUMENT('Expected terminal to be a Terminal instance', { name: 'opts.terminal', scope: 'CLI.exec', value: opts.terminal });
+		}
+
+		let { showHelpOnError } = this;
+		let exitCode = undefined;
+		const parser = new Parser(opts);
+		const __argv = _argv.slice(0);
+		const results = {
+			_:           undefined,
+			_argv,       // the original unparsed arguments
+			__argv,      // the parsed arguments
+			argv:        undefined,
+			cli:         this,
+			cmd:         undefined,
+			console:     opts.terminal.console,
+			contexts:    undefined,
+			data:        opts.data,
+			exitCode:    opts.exitCode = code => code === undefined ? exitCode : (exitCode = code || 0),
+			help:        () => renderHelp(results.cmd),
+			result:      undefined,
+			terminal:    opts.terminal,
+			unknown:     undefined,
+			warnings:    this.warnings
+		};
+
 		this.once('banner', ({ argv, ctx = this }) => {
-			if (this.get('autoHideBanner') !== false) {
-				(opts.terminal || this.get('terminal')).once('output', () => {
+			if (this.autoHideBanner) {
+				opts.terminal.once('output', () => {
 					let banner = ctx.prop('banner');
 					if (banner && this.bannerEnabled) {
 						banner = String(typeof banner === 'function' ? banner(opts) : banner).trim();
-						(opts.terminal || this.get('terminal')).stdout.write(`${banner}\n\n`);
+						opts.terminal.stdout.write(`${banner}\n\n`);
 					}
 				});
 			}
+
 			if ((argv && !argv.banner) || (ctx instanceof Extension && !ctx.isCLIKitExtension && !ctx.get('showBannerForExternalCLIs'))) {
 				this.bannerEnabled = false;
 			} else if (ctx.banner) {
@@ -248,29 +355,8 @@ export default class CLI extends Context {
 			}
 		});
 
-		let { showHelpOnError } = this;
-		let exitCode = undefined;
-		const parser = new Parser();
-		const results = {
-			_:           undefined,
-			__argv:      undefined,
-			argv:        undefined,
-			cli:         this,
-			clikit:      { ...require('./index') },
-			cmd:         undefined,
-			console:     (opts.terminal || this.terminal).console,
-			contexts:    undefined,
-			data:        opts.data,
-			exitCode:    opts.exitCode = code => code === undefined ? exitCode : (exitCode = code || 0),
-			help:        () => renderHelp(results.cmd),
-			result:      undefined,
-			unknown:     undefined,
-			warnings:    this.warnings
-		};
-
 		try {
-			const __argv = unparsedArgs ? Array.prototype.concat.apply([], unparsedArgs.map(a => a.input || a)) : process.argv.slice(2);
-			const { _, argv, contexts, unknown } = await parser.parse(unparsedArgs || process.argv.slice(2), this, opts);
+			const { _, argv, contexts, unknown } = await parser.parse(__argv, opts.clone ? new Context(this) : this);
 
 			log('Parsing complete: ' +
 				`${pluralize('option', Object.keys(argv).length, true)}, ` +
@@ -283,7 +369,6 @@ export default class CLI extends Context {
 			const cmd = contexts[0];
 
 			results._        = _;
-			results.__argv   = __argv;
 			results.argv     = argv;
 			results.cmd      = cmd;
 			results.contexts = contexts;
@@ -344,6 +429,105 @@ export default class CLI extends Context {
 
 			throw err;
 		}
+	}
+
+	/**
+	 * Starts a WebSocket server.
+	 *
+	 * @param {Object} opts - WebSocket server options. Visit
+	 * https://github.com/websockets/ws/blob/HEAD/doc/ws.md#new-websocketserveroptions-callback for
+	 * more information.
+	 * @returns {Promise.<WebSocketServer>}
+	 * @access public
+	 */
+	listen(opts) {
+		return new Promise(resolve => {
+			log('Starting WebSocketServer...');
+			this.connections = {};
+			this.server = new WebSocketServer(opts, () => resolve(this.server));
+
+			this.server.on('connection', (ws, req) => {
+				const { remoteAddress, remotePort } = req.socket;
+				const key = remoteAddress + ':' + remotePort;
+				log('%s upgraded to WebSocket', highlight(key));
+				const { headers } = req;
+				log(headers);
+
+				const stdout = new OutputSocket(ws);
+				const stderr = new OutputSocket(ws);
+				let echo = true;
+				const terminal = new Terminal({ stdout, stderr });
+
+				const run = async argv => {
+					log('Running:', argv);
+					await this.exec(argv, { terminal });
+				};
+
+				// TODO: get the cwd, env, argv from headers
+
+				const conn = this.connections[key] = {
+					buffer: '',
+					cols: 0,
+					rows: 0
+				};
+
+				ws.on('close', () => {
+					delete this.connections[key];
+					log('%s closed WebSocket', highlight(key));
+				});
+
+				ws.on('error', err => {
+					delete this.connections[key];
+					if (err.code !== 'ECONNRESET') {
+						error(err);
+					}
+				});
+
+				ws.on('message', async msg => {
+					if (Buffer.isBuffer(msg)) {
+						msg = msg.toString();
+					}
+
+					const pos = msg.match(ansi.cursor.position);
+					if (pos) {
+						conn.rows = ~~pos[1];
+						conn.cols = ~~pos[2];
+						log(`Terminal set to ${conn.cols} x ${conn.rows}`);
+						return;
+					}
+
+					msg = msg.replace(/\r\n|\n|\r/g, '\r\n');
+					conn.buffer += msg;
+
+					if (echo) {
+						ws.send(msg);
+					}
+
+					let p = conn.buffer.indexOf('\r');
+					while (p !== -1) {
+						const argv = split(conn.buffer.substring(0, p));
+						conn.buffer = conn.buffer.substring(p + 2);
+
+						if (argv.length) {
+							await run(argv);
+						}
+
+						p = conn.buffer.indexOf('\r');
+					}
+				});
+
+				ws.send(ansi.cursor.save);
+				ws.send(ansi.cursor.move(999, 999));
+				ws.send(ansi.cursor.get);
+				ws.send(ansi.cursor.restore);
+
+				// if (headers.argv !== undefined) {
+				// 	run(headers.argv);
+				// }
+			});
+
+			this.server.on('error', error);
+		});
 	}
 
 	/**

src/parser/parser.js
@@ -44,9 +44,14 @@ export default class Parser {
 	/**
 	 * Initializes the internal properties and class name.
 	 *
+	 * @param {Object} [opts] - Various options.
+	 * @param {Object} [opts.data] - User-defined data to pass into the selected command.
+	 * @param {Function} [opts.exitCode] - A function that sets the exit code.
+	 * @param {Termianl} [opts.terminal] - A terminal instance to override the default CLI terminal
+	 * instance.
 	 * @access public
 	 */
-	constructor() {
+	constructor(opts = {}) {
 		Object.defineProperties(this, {
 			/**
 			 * The array of arguments. As arguments are identified, they are replaced with
@@ -68,6 +73,15 @@ export default class Parser {
 			 */
 			contexts: {
 				value: []
+			},
+
+			/**
+			 * Options possibly containing a `data` payload, `exitCode`, and `terminal` instance.
+			 *
+			 * @type {Object}
+			 */
+			opts: {
+				value: opts
 			}
 		});
 
@@ -168,15 +182,10 @@ export default class Parser {
 	 *
 	 * @param {Array} args - An array of raw, unparsed arguments.
 	 * @param {Context} ctx - The context to reference for commands, options, and arguments.
-	 * @param {Object} opts - Various options.
-	 * @param {Object} opts.data - User-defined data to pass into the selected command.
-	 * @param {Function} opts.exitCode - A function that sets the exit code.
-	 * @param {Termianl} opts.terminal - A terminal instance to override the default CLI terminal
-	 * instance.
 	 * @returns {Promise<Parser>}
 	 * @access public
 	 */
-	parse(args, ctx, opts = {}) {
+	parse(args, ctx) {
 		return ctx.hook('parse', async args => {
 			if (!Array.isArray(args)) {
 				throw E.INVALID_ARGUMENT('Expected args to be an array', { name: 'args', scope: 'Parser.parse', value: args });
@@ -188,8 +197,6 @@ export default class Parser {
 
 			this.args = args;
 			log(`Processing ${pluralize('argument', args.length, true)}: ${highlight(this.args.join(', '))}`);
-
-			this.opts = opts || {};
 
 			// add the context to the stack
 			this.contexts.unshift(ctx);
@@ -388,6 +395,20 @@ export default class Parser {
 		}
 
 		await this.parseArg(ctx, 0);
+
+		// if there are no more contexts to descend, check if the top-most context is actually
+		// a default subcommand
+		let cmd = this.contexts[0];
+		if (cmd === ctx && cmd.action instanceof Command) {
+			cmd = cmd.action;
+			cmd.link(ctx);
+			this.contexts.unshift(cmd);
+		}
+
+		if (cmd !== ctx) {
+			log('Descending into next context\'s parser');
+			await this.parseWithContext(cmd);
+		}
 	}
 
 	/**
@@ -395,31 +416,25 @@ export default class Parser {
 	 *
 	 * @param {CLI|Command} ctx - The context to apply when parsing the command line arguments.
 	 * @param {Number} i - The argument index number to parse.
+	 * @param {Number} [to] - The index to go until.
 	 * @returns {Promise}
 	 * @access private
 	 */
-	async parseArg(ctx, i) {
-		if (i === this.args.length) {
-			let cmd = this.contexts[0];
-
-			// if there are no more contexts to descend, check if the top-most context is actually
-			// a default subcommand
-			if (cmd === ctx && cmd.action instanceof Command) {
-				cmd = cmd.action;
-				cmd.link(ctx);
-				this.contexts.unshift(cmd);
-			}
-
-			if (cmd !== ctx) {
-				log('Descending into next context\'s parser');
-				return this.parseWithContext(cmd);
-			}
-
-			log('End of the line');
+	async parseArg(ctx, i, to) {
+		if (i >= (to !== undefined ? to : this.args.length)) {
 			return;
 		}
 
+		let { rev } = ctx;
+		let { length } = this.args;
+
+		// create a ParsedArgument object for the next argument
 		const arg = await this.createParsedArgument(ctx, i);
+
+		if (to !== undefined && this.args.length < length) {
+			to -= (length - this.args.length);
+		}
+
 		if (arg) {
 			if ((arg.type !== 'command' && arg.type !== 'extension') || this.contexts[0] === ctx) {
 				this.args[i] = arg;
@@ -441,64 +456,71 @@ export default class Parser {
 				this.contexts.unshift(arg.extension);
 
 			} else if (arg.type === 'option' && typeof arg.option.callback === 'function' && !arg.option.multiple) {
-				return new Promise((resolve, reject) => {
-					const { option } = arg;
-					log(`Firing option ${highlight(option.format)} callback ${note(`(${option.parent.name})`)}`);
-					let fired = false;
+				const { option } = arg;
+				log(`Firing option ${highlight(option.format)} callback ${note(`(${option.parent.name})`)}`);
+				let fired = false;
 
-					Promise.resolve()
-						.then(() => option.callback({
-							ctx,
-							exitCode: this.opts.exitCode,
-							input: arg.input,
-							next: async () => {
-								if (fired) {
-									log('next() already fired');
-									return;
-								}
-
-								fired = true;
-
-								log(`Option ${highlight(option.format)} called next(), processing next arg`);
-								await this.parseArg(ctx, i + 1);
-
-								return this.args[i].value;
-							},
-							opts: this.opts,
-							option,
-							value: arg.value
-						}))
-						.then(async value => {
-							if (value === undefined) {
-								log(`Option ${highlight(option.format)} callback did not change the value`);
-							} else {
-								log(`Option ${highlight(option.format)} callback changed value ${highlight(arg.value)} to ${highlight(value)}`);
-								arg.value = value;
-							}
-
-							if (!fired) {
-								log(`Option ${highlight(option.format)} did not call next(), processing next arg`);
-								await this.parseArg(ctx, i + 1);
-							}
-
-							return arg.value;
-						})
-						.catch(err => {
-							if (err.code === 'ERR_NOT_AN_OPTION') {
-								this.args[i] = new ParsedArgument('argument', {
-									input: arg.input
-								});
+				try {
+					const value = await option.callback({
+						ctx,
+						exitCode: this.opts.exitCode,
+						input: arg.input,
+						next: async () => {
+							if (fired) {
+								log('next() already fired');
 								return;
 							}
-							throw err;
-						})
-						.then(resolve)
-						.catch(reject);
-				});
+
+							fired = true;
+
+							log(`Option ${highlight(option.format)} called next(), processing next arg`);
+
+							const { value } = this.args[i];
+
+							if (ctx.rev > rev) {
+								log(`Rev changed from ${highlight(rev)} to ${highlight(ctx.rev)}, reparsing ${highlight(0)} through ${highlight(i - 1)}`);
+								await this.parseArg(ctx, 0, i);
+							}
+
+							await this.parseArg(ctx, i + 1, to);
+
+							return value;
+						},
+						opts: this.opts,
+						option,
+						value: arg.value
+					});
+
+					if (value === undefined) {
+						log(`Option ${highlight(option.format)} callback did not change the value`);
+					} else {
+						log(`Option ${highlight(option.format)} callback changed value ${highlight(arg.value)} to ${highlight(value)}`);
+						arg.value = value;
+					}
+
+					if (fired) {
+						return;
+					}
+
+					log(`Option ${highlight(option.format)} did not call next(), processing next arg`);
+				} catch (err) {
+					if (err.code !== 'ERR_NOT_AN_OPTION') {
+						throw err;
+					}
+
+					this.args[i] = new ParsedArgument('argument', {
+						input: arg.input
+					});
+				}
 			}
 		}
 
-		return this.parseArg(ctx, i + 1);
+		if (ctx.rev > rev) {
+			log(`Rev changed from ${highlight(rev)} to ${highlight(ctx.rev)}, reparsing ${highlight(0)} through ${highlight(i - 1)}`);
+			await this.parseArg(ctx, 0, i);
+		}
+
+		await this.parseArg(ctx, i + 1, to);
 	}
 
 	/**
@@ -644,7 +666,7 @@ export default class Parser {
 		} else {
 			const ext = lookup.extensions[subject];
 			if (ext) {
-				await ext.load(args.slice(i + 1));
+				await ext.load();
 				log(`Found extension: ${highlight(ext.name)}`);
 				return new ParsedArgument('extension', {
 					extension: ext,

site/src/components/term.js
@@ -1,9 +1,59 @@
 import React, { PureComponent } from 'react'
 import { Terminal } from 'xterm';
-import 'xterm/dist/xterm.css';
-import * as fit from 'xterm/lib/addons/fit/fit';
+import 'xterm/css/xterm.css';
+// import { AttachAddon } from 'xterm-addon-attach';
+import { FitAddon } from 'xterm-addon-fit';
 
-Terminal.applyAddon(fit);
+class AttachAddon {
+	constructor() {
+		this.ws = new WebSocket('ws://localhost:12345');
+		this.ws.binaryType = 'arraybuffer';
+		this.disposables = [];
+	}
+
+	activate(term) {
+		this.disposables.push(
+			this.addListener('message', evt => {
+				console.log(evt);
+				const { data } = evt;
+				term.write(typeof data === 'string' ? data : new Uint8Array(data));
+			}),
+			this.addListener('close', () => this.dispose()),
+			this.addListener('error', () => this.dispose()),
+			term.onData(data => this.sendData(data)),
+			term.onBinary(data => this.sendBinary(data))
+		);
+	}
+
+	addListener(type, handler) {
+		this.ws.addEventListener(type, handler);
+		return {
+			dispose: () => this.ws.removeEventListener(type, handler)
+		};
+	}
+
+	dispose() {
+		this.disposables.forEach(d => d.dispose());
+	}
+
+	sendBinary(data) {
+		console.log('BINARY', data);
+		if (this.ws.readyState === 1) {
+			const buffer = new Uint8Array(data.length);
+			for (let i = 0; i < data.length; i++) {
+				buffer[i] = data.charCodeAt(i) & 255;
+			}
+			this.ws.send(buffer);
+		}
+	}
+
+	sendData(data) {
+		console.log('DATA', data);
+		if (this.ws.readyState === 1) {
+			this.ws.send(data);
+		}
+	}
+}
 
 export default class Term extends PureComponent {
 	container = React.createRef();
@@ -16,14 +66,26 @@ export default class Term extends PureComponent {
 				background: '#1e1e1e'
 			}
 		});
+
+		// const ws = new WebSocket('ws://localhost:12345');
+		const attachAddon = new AttachAddon(); // ws);
+		const fitAddon = new FitAddon();
+
+		term.loadAddon(attachAddon);
+		term.loadAddon(fitAddon);
+
 		term.open(this.container.current);
-		term.fit();
-		term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+
+		fitAddon.fit();
+
+		console.log(term.rows, term.cols);
+
+		// term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
 	}
 
 	componentWillUnmount() {
 		if (this.term) {
-			this.term.destroy();
+			this.term.dispose();
 			this.term = null;
 		}
 	}

src/commands/help.js
@@ -13,7 +13,7 @@ const { highlight } = debug.styles;
  * @returns {String}
  */
 export async function renderHelp(ctx) {
-	const file = ctx.get('helpTemplate', path.resolve(__dirname, '..', '..', 'templates', 'help.tpl'));
+	const file = ctx.get('helpTemplateFile', path.resolve(__dirname, '..', '..', 'templates', 'help.tpl'));
 	log(`Rendering help template: ${highlight(file)}`);
 	return renderFile(file, await ctx.generateHelp());
 }
@@ -92,7 +92,7 @@ export default {
 				_stdout.write(JSON.stringify(help, null, '  '));
 				_stdout.write('\n');
 			} else {
-				const file = ctx.get('helpTemplate', path.resolve(__dirname, '..', '..', 'templates', 'help.tpl'));
+				const file = ctx.get('helpTemplateFile', path.resolve(__dirname, '..', '..', 'templates', 'help.tpl'));
 				log(`Rendering help template: ${highlight(file)}`);
 				// determine the output stream
 				(err ? _stderr : _stdout).write(renderFile(file, help));

src/index.js
@@ -20,8 +20,6 @@ import snooplogg from 'snooplogg';
 
 export default CLI;
 
-export const terminal = new Terminal();
-
 export {
 	Argument,
 	CLI,

src/lib/ansi.js
@@ -5,8 +5,8 @@ export const clear = '\x1bc';
 export const cursor = {
 	show:    '\x1b[?25h',
 	hide:    '\x1b[?25l',
-	save:    '\x1b[7',
-	restore: '\x1b[8',
+	save:    '\x1b7',
+	restore: '\x1b8',
 	get:     '\x1b[6n',
 	home:    '\x1b[H',
 	left:    '\x1b[G',
@@ -28,6 +28,8 @@ export const cursor = {
 		return y ? `\x1b[${y + 1};${x + 1}H` : `\x1b[${x + 1}G`;
 	},
 
+	position: /^\x1b\[(\d+);(\d+)R$/,
+
 	next(n = 1) { return '\x1b[E'.repeat(n); },
 	prev(n = 1) { return '\x1b[F'.repeat(n); }
 };
@@ -47,6 +49,8 @@ export const erase = {
 	toStart: '\x1b[1K',
 	up:      '\x1b[1J'
 };
+
+export const esc = '\x1b[';
 
 export const scroll = {
 	down: '\x1b[T',

src/parser/argument-list.js
@@ -1,5 +1,4 @@
 import Argument from './argument';
-import E from '../lib/errors';
 
 import { declareCLIKitClass } from '../lib/util';
 

src/parser/context.js
@@ -72,6 +72,7 @@ export default class Context extends HookEmitter {
 	 */
 	argument(arg) {
 		this.args.add(arg);
+		this.rev++;
 		return this;
 	}
 
@@ -94,6 +95,7 @@ export default class Context extends HookEmitter {
 			log(`Adding command: ${highlight(cmd.name)}`);
 			this.register(cmd);
 		}
+		this.rev++;
 		return this;
 	}
 
@@ -116,6 +118,7 @@ export default class Context extends HookEmitter {
 			log(`Adding extension: ${highlight(ext.name)}`);
 			this.register(ext);
 		}
+		this.rev++;
 		return this;
 	}
 
@@ -268,6 +271,7 @@ export default class Context extends HookEmitter {
 		this.name       = params.name;
 		this.options    = new OptionMap();
 		this.parent     = params.parent;
+		this.rev        = 0;
 		this.title      = params.title || params.name;
 		this.treatUnknownOptionsAsArguments = !!params.treatUnknownOptionsAsArguments;
 
@@ -327,6 +331,8 @@ export default class Context extends HookEmitter {
 				}
 			}
 		}
+
+		this.rev++;
 
 		return this;
 	}

commit 83bff1988d19f68d160f84342d0e878a18a4c942

fix(parser): Fixed regression introduced in 24f5048 where parser context chains where processed one context at a time instead of chaining them all together.

src/cli.js
@@ -179,7 +179,7 @@ export default class CLI extends Context {
 		if (this.version && !this.lookup.short.v && !this.lookup.long.version) {
 			this.option('-v, --version', {
 				callback: async ({ exitCode, opts, next }) => {
-					if (await next()) {
+					if (await next() === true) {
 						let version = this.version;
 						if (typeof version === 'function') {
 							version = await version(opts);

src/parser/parser.js
@@ -402,20 +402,6 @@ export default class Parser {
 		}
 
 		await this.parseArg(ctx, 0);
-
-		// if there are no more contexts to descend, check if the top-most context is actually
-		// a default subcommand
-		let cmd = this.contexts[0];
-		if (cmd === ctx && cmd.action instanceof Command) {
-			cmd = cmd.action;
-			cmd.link(ctx);
-			this.contexts.unshift(cmd);
-		}
-
-		if (cmd !== ctx) {
-			log('Descending into next context\'s parser');
-			await this.parseWithContext(cmd);
-		}
 	}
 
 	/**
@@ -428,7 +414,26 @@ export default class Parser {
 	 * @access private
 	 */
 	async parseArg(ctx, i, to) {
-		if (i >= (to !== undefined ? to : this.args.length)) {
+		if (to !== undefined && i >= to) {
+			// all caught up, return
+			return;
+		} else if (to === undefined && i >= this.args.length) {
+			let cmd = this.contexts[0];
+
+			// if there are no more contexts to descend, check if the top-most context is actually
+			// a default subcommand
+			if (cmd === ctx && cmd.action instanceof Command) {
+				cmd = cmd.action;
+				cmd.link(ctx);
+				this.contexts.unshift(cmd);
+			}
+
+			if (cmd !== ctx) {
+				log('Descending into next context\'s parser');
+				return this.parseWithContext(cmd);
+			}
+
+			log('End of the line');
 			return;
 		}
 
@@ -482,8 +487,7 @@ export default class Parser {
 
 							log(`Option ${highlight(option.format)} called next(), processing next arg`);
 
-							const { value } = this.args[i];
-
+							// check if the option callback added any new commands, options, or arguments
 							if (ctx.rev > rev) {
 								log(`Rev changed from ${highlight(rev)} to ${highlight(ctx.rev)}, reparsing ${highlight(0)} through ${highlight(i - 1)}`);
 								await this.parseArg(ctx, 0, i);
@@ -491,7 +495,7 @@ export default class Parser {
 
 							await this.parseArg(ctx, i + 1, to);
 
-							return value;
+							return this.args[i].value;
 						},
 						opts: this.opts,
 						option,